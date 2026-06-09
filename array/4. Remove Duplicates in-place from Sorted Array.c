cd6149dd361da9b288794ec9065e1b373069f301
PrakritiPandey24 <prakritip2023@gmail.com>
2026-06-09 16:35:38 +0530

    Update 4. Remove Duplicates in-place from Sorted Array.c

--- array/4. Remove Duplicates in-place from Sorted Array.c
+++ array/4. Remove Duplicates in-place from Sorted Array.c
@@ -1,18 +1,21 @@
 /*
-LC 26
-Remove Duplicates in-place from Sorted Array
+LC 26 - Remove Duplicates from Sorted Array
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+
 */
 
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
         int left = 0;
-        for(int i = 0; i < nums.size(); i++){
-            if(nums[left] != nums[i]){
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[left] != nums[i]) {
                 left++;
                 nums[left] = nums[i];
             }
         }
-        return left+1;
+        return left + 1;
     }
 };
