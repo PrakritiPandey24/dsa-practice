303cf32e1fdade914a49c3d0f418bf6ff6b06f21
PrakritiPandey24 <prakritip2023@gmail.com>
2026-06-09 16:34:46 +0530

    Add Rotate Array solution
    Implement rotate function to rotate an array by k positions.

---
+++ array/5. Rotate Array.c
@@ -0,0 +1,17 @@
+/*
+LC 189 - Rotate Array
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+
+*/
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        k %= nums.size(); // handle k > n -> to loop around if needed. 
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin() + k);
+        reverse(nums.begin() + k, nums.end());
+    }
+};
