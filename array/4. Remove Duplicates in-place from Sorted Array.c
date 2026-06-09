42b2b75ae56ba33c913f84576df7df3ccab89b5f
PrakritiPandey24 <prakritip2023@gmail.com>
2026-06-09 16:24:29 +0530

    Add removeDuplicates function for sorted array
    Implement function to remove duplicates from sorted array in-place.

---
+++ array/4. Remove Duplicates in-place from Sorted Array.c
@@ -0,0 +1,18 @@
+/*
+LC 
+Remove Duplicates in-place from Sorted Array
+*/
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int left = 0;
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[left] != nums[i]){
+                left++;
+                nums[left] = nums[i];
+            }
+        }
+        return left+1;
+    }
+};
