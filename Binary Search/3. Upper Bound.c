d83087509ed65c551a6c0a0cc066a9631d78be67
PrakritiPandey24 <prakritip2023@gmail.com>
2026-06-13 22:36:40 +0530

    Create 3. Upper Bound.c

---
+++ Binary Search/3. Upper Bound.c
@@ -0,0 +1,19 @@
+/*
+Upper Bound
+Time Complexity: O(log n)
+Space Complexity:O(1)
+*/
+
+int SearchUpper(vector<int>& nums, int target){
+  int low = 0, high = nums.size()-1, ans = nums.size();
+  while(low <= high){
+    int mid = low+(high-low)/2;
+    if(nums[mid]<=target){
+      low = mid+1;
+    }else{
+      ans = mid;
+      high = mid-1;
+    }
+  }
+  return ans;
+}
