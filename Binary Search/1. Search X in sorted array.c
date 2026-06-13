cbf7a356b297973a5e0112e1f7cbdccbfee3f744
PrakritiPandey24 <prakritip2023@gmail.com>
2026-06-13 22:16:08 +0530

    Create 1. Search X in sorted array.c

---
+++ Binary Search/1. Search X in sorted array.c
@@ -0,0 +1,22 @@
+/*
+LC 704. Binary Search
+Time Complexity: O(log n)
+Space Complexity: O(1)
+*/
+
+    int search(vector<int>& nums, int target) {
+        int low = 0, high = nums.size()-1;
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            if(nums[mid] == target){
+                return mid;
+            }else if(nums[mid]> target){
+                high = mid-1;
+            }else{
+                low = mid+1;
+            }
+        }
+        return -1;
+    }
+
+
