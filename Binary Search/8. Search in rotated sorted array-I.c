58ffe678a1d32554865b77ca8821f9503beb2d06
PrakritiPandey24 <prakritip2023@gmail.com>
2026-06-15 12:28:03 +0530

    Create 8. Search in rotated sorted array-I.c

---
+++ Binary Search/8. Search in rotated sorted array-I.c
@@ -0,0 +1,31 @@
+/*
+LC 33. Search in Rotated Sorted Array
+Time Complexity: O(log n)
+Space Complexity: O(1)
+*/
+
+int search(vector<int>& nums, int target) {
+        int low = 0, high = nums.size()-1;
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            if(nums[low] <= nums[mid])
+            {
+                if(target >= nums[low] && target < nums[mid])
+                    high = mid-1;
+                else
+                    low = mid+1;
+            }
+            else
+            {
+                if(nums[mid] < target && target <= nums[high])
+                    low = mid+1;
+                else
+                    high = mid-1;
+            }
+
+        }
+        return -1;
+}
